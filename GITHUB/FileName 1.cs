7e574fcf86cc6c07e0ee4b5fa7e2d1df63c20919
Alessandro De Maria <Alessandro.demariagbg@gmail.com>
2025-09-17 09:53:46 +0200

    övning 3

---
+++ GITHUB/FileName 1.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * 3.   Skriv ett uttryck som beräknar arean av en rektangel utifrån inmatade bredd och höjd
+*/
+
+Console.Write("Ange rektangelns bredd: ");
+double bredd = double.Parse(Console.ReadLine());
+
+Console.Write("Ange rektangelns höjd: ");
+double höjd = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Rektangelns area: {bredd * höjd}");
---
+++ GITHUB/FileName.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * 2.   Skriv ett booleskt uttryck som kollar om ett givet heltal är jämnt delbart 
+ *      med 5 och 7 samtidigt
+*/
+
+Console.Write("Ange ett heltal: ");
+int number = int.Parse(Console.ReadLine());
+
+//bool isDivisible = number % 35 == 0;
+bool isDivisible = (number % 5 == 0) && (number % 7 == 0);
+
+Console.WriteLine("Talet {0} är delbart med 5 och 7 samtidigt: {1}", number, isDivisible);
+//Console.WriteLine("Talet {0} är delbart med 5 och 7 samtidigt: {1}", number, number % 35 == 0);
+//Console.WriteLine("Talet {0} är delbart med 5 och 7 samtidigt: {1}", number, 
+//    (number % 5 == 0) && (number % 7 == 0));
